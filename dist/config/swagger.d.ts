ff070ba59edc0db7bd4c4e0094cd5acfb825ceb8
brayan <brodriguez20071@gmail.com>
2025-09-30 15:16:31 -0500

    Docs(eventos): Agregfar sawwer a las rutas

--- dist/config/swagger.d.ts
+++
@@ -1,8 +0,0 @@
-import swaggerUi from "swagger-ui-express";
-declare const swaggerSpec: object;
-declare const swaggerUiOptions: {
-    customCss: string;
-    customSiteTitle: string;
-    customfavIcon: string;
-};
-export { swaggerSpec, swaggerUi, swaggerUiOptions };
